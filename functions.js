e0dea2cbf262fc9337a5c0f26d0177851d508412
Niket Bahety <niketbahety@gmail.com>
2022-02-27 20:28:42 +0530

    Updated Word Check Feature

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ let checked = false;
 let word = "";
 let letters = document.getElementsByClassName("letters");
 let originalWord = "SPILL";
+let gameWon = false;
 
 axios
   .get("https://randomwords-api.herokuapp.com/api/v1/multiple?length=5")
@@ -15,6 +16,19 @@ axios
     console.log(err);
   });
 
+async function wordCheck(word) {
+  let res;
+  res = await axios.get(
+    `https://randomwords-api.herokuapp.com/api/v1/check/${word}`
+  );
+  if (res.data.data.define.definition) {
+    console.log(res.data.data.define.definition);
+    return res.data.data.define.definition;
+  } else {
+    return false;
+  }
+}
+
 function deleteLetter(e) {
   if (count > 0 && word.length != 0) {
     checked = false;
@@ -25,47 +39,51 @@ function deleteLetter(e) {
     word = word.slice(0, -1);
   }
 }
-function submitLetter(e) {
+async function submitLetter(e) {
   if (count % 5 === 0 && count != 0 && word.length != 0) {
-    console.log("Entered");
-    console.log(word);
-    checked = false;
-    let arr = [];
-    for (let i = 0; i < 5; i++) {
-      if (word[i] === originalWord[i]) arr.push("green");
-      else if (originalWord.includes(word[i])) arr.push("rgb(255, 140, 0)");
-      else arr.push("#333");
+    let def = await wordCheck(word);
+    if (def) {
+      checked = false;
+      let arr = [];
+      for (let i = 0; i < 5; i++) {
+        if (word[i] === originalWord[i]) arr.push("green");
+        else if (originalWord.includes(word[i])) arr.push("rgb(255, 140, 0)");
+        else arr.push("#333");
+      }
+      for (let i = count - 5; i < count; i++) {
+        console.log(letters[i]);
+        letters[i].style.animation = "flip 1s";
+        letters[i].style.animationDelay = (i % 5) * 0.25 + "s";
+        letters[i].children[0].style.animation = "reverse-flip 1s";
+        letters[i].children[0].style.animationDelay = (i % 5) * 0.25 + "s";
+      }
+      let x = 250;
+      for (let i = count - 5; i < count; i++) {
+        setTimeout(function () {
+          letters[i].style.backgroundColor = arr[i % 5];
+        }, x);
+        x = x + 250;
+      }
+      console.log(arr);
+      if (word === originalWord) {
+        setTimeout(function () {
+          alert(`Victory\nMeaning: ${def}`);
+          gameWon = true;
+        }, 2000);
+      } else if (count === 30) {
+        setTimeout(function () {
+          alert(`Lost !! Word was ${originalWord}`);
+        }, 2000);
+      }
+      word = "";
+    } else {
+      alert("Invalid Word");
     }
-    for (let i = count - 5; i < count; i++) {
-      console.log(letters[i]);
-      letters[i].style.animation = "flip 1s";
-      letters[i].style.animationDelay = (i % 5) * 0.25 + "s";
-      letters[i].children[0].style.animation = "reverse-flip 1s";
-      letters[i].children[0].style.animationDelay = (i % 5) * 0.25 + "s";
-    }
-    let x = 250;
-    for (let i = count - 5; i < count; i++) {
-      setTimeout(function () {
-        letters[i].style.backgroundColor = arr[i % 5];
-      }, x);
-      x = x + 250;
-    }
-    console.log(arr);
-    if (word === originalWord) {
-      setTimeout(function () {
-        alert("Victory");
-      }, 2000);
-    } else if (count === 30) {
-      setTimeout(function () {
-        alert(`Lost !! Word was ${originalWord}`);
-      }, 2000);
-    }
-    word = "";
   }
 }
 
 function writeLetter(e) {
-  if (!checked) {
+  if (!checked && !gameWon) {
     let letter = letters[count];
     let p = document.createElement("p");
     p.innerHTML = String.fromCharCode(e.keyCode);
