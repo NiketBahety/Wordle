5a9086a219123cac009545162675521416ff19d7
Niket Bahety <niketbahety@gmail.com>
2022-03-01 18:47:41 +0530

    Added keyboard

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let keys = document.getElementsByClassName("key");
+
+for (let i = 0; i < 28; i++) {
+  keys[i].addEventListener("click", function () {
+    var e = new KeyboardEvent("keyup", {
+      bubbles: true,
+      cancelable: true,
+      key: keys[i].innerHTML,
+      char: keys[i].innerHTML,
+      shiftKey: true,
+    });
+    delete e.keyCode;
+    let val = keys[i].innerHTML.charCodeAt(0);
+    if (val === 60) val = 8;
+    else if (val === 10) val = 13;
+    Object.defineProperty(e, "keyCode", {
+      value: val,
+    });
+    document.dispatchEvent(e);
+  });
+}
+
 document.addEventListener("keyup", function (e) {
   if (e.keyCode == 8) {
     deleteLetter(e);
